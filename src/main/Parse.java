27cb279307ca6b79f22f64af24cb9a5fa5def85c
mixheaven <mixheaven@hotmail.com>
2020-01-05 19:14:32 +0100

    finalshoot

--- src/main/Parse.java
+++ src/main/Parse.java
@@ -1,6 +1,8 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+
+
 import java.io.File;
 import java.io.IOException;
 
@@ -9,25 +11,28 @@ public class Parse {
     private final static String JSON_WEATHER_PATH = "weather.json";
 
     public static void main(String[] args) {
-        ObjectMapper objectMapper = new ObjectMapper();
+
         try {
             // write your code here !
+            ObjectMapper objectMapper = new ObjectMapper();
 
             // TODO : get the root from the file JSON_WEATHER_PATH
-            JsonNode root = null;
+            JsonNode root = objectMapper.readTree(new File(JSON_WEATHER_PATH));
 
             // TODO : get the value of "name" attribute
-            String cityName = null;
 
+            String cityName = root.get("name").asText();
             // TODO : get the "lat" and "lon" values of the "coord"
-            Double cityLatitude = null;
-            Double cityLongitude = null;
+            JsonNode coord = root.path("coord");
+            double cityLatitude = coord.get("lat").asDouble();
+
+            double cityLongitude = coord.get("lon").asDouble();
 
             // TODO : get the "wind" attribute as an Wind object
-            Wind wind = null;
+            JsonNode wind = root.get("wind");
 
             // TODO : get the "weather" attribute as an array of Weather objects
-            Weather[] weathers = {};
+            Weather[] weathers = objectMapper.convertValue(root.withArray("weather"), Weather[].class);
 
             // Don't touch this !
             System.out.printf("City name: %s%n", cityName);
@@ -38,15 +43,14 @@ public class Parse {
                 System.out.printf("Weather infos: %s%n", weather.toString());
             }
             /*
-                Expected result :
-
-                City name: London
-                City latitude: 51.51
-                City longitude: -0.13
-                Wind infos: src.main.Wind{speed=4.1, deg=80.0}
-                Weather infos: src.main.Weather{id=300, main='Drizzle', description='light intensity drizzle', icon='09d'}
-                Weather infos: src.main.Weather{id=800, main='Clear', description='clear sky', icon='01n'}
-            */
+             * Expected result :
+             * 
+             * City name: London City latitude: 51.51 City longitude: -0.13 Wind infos:
+             * src.main.Wind{speed=4.1, deg=80.0} Weather infos: src.main.Weather{id=300,
+             * main='Drizzle', description='light intensity drizzle', icon='09d'} Weather
+             * infos: src.main.Weather{id=800, main='Clear', description='clear sky',
+             * icon='01n'}
+             */
         } catch (IOException e) {
             e.printStackTrace();
         }
